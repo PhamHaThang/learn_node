2d3a00af3320acff79349f8c10e1453d91978bc2
ThangPH <hathang2004@gmail.com>
2025-08-22 12:57:30 +0700

    Update tree lisst

--- controllers/admin/products-category.controller.js
+++ controllers/admin/products-category.controller.js
@@ -1,21 +1,27 @@
 const ProductCategory = require("../../models/product-category.model");
 const systemConfig = require("../../config/systems");
+const createTree = require("../../helpers/createTree");
 // [GET] /admin/products-category
 module.exports.index = async (req, res) => {
   const find = {
     deleted: false,
   };
-  const records = await ProductCategory.find(find);
 
+  const records = await ProductCategory.find(find);
+  const newRecords = createTree(records);
   res.render("admin/pages/products-category/index", {
     pageTitle: "Danh mục sản phẩm",
-    records: records,
+    records: newRecords,
   });
 };
 // [GET] /admin/products-category/create
-module.exports.create = (req, res) => {
+module.exports.create = async (req, res) => {
+  let find = { deleted: false };
+  const records = await ProductCategory.find(find).sort({});
+  const newRecords = createTree(records);
   res.render("admin/pages/products-category/create", {
     pageTitle: "Tạo danh mục sản phẩm",
+    records: newRecords,
   });
 };
 // [POST] /admin/products-category/create
