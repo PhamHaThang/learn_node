ee45df3dbf637d06e11bfdf81179afa2825fd940
ThangPH <hathang2004@gmail.com>
2025-08-25 21:31:44 +0700

    authorize

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,16 +1,25 @@
 const systemConfig = require("../../config/systems");
 const Account = require("../../models/account.model");
+const Role = require("../../models/roles.model");
 module.exports.requireAuth = async (req, res, next) => {
   if (!req.cookies.token) {
     res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     return;
   } else {
     const token = req.cookies.token;
-    const user = await Account.findOne({ token: token, deleted: false });
+    const user = await Account.findOne({ token: token, deleted: false }).select(
+      "-password"
+    );
     if (!user) {
       res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
       return;
     } else {
+      const role = await Role.findOne({
+        _id: user.role_id,
+        deleted: false,
+      }).select("title permissions");
+      res.locals.user = user;
+      res.locals.role = role;
       next();
     }
   }
