e993b1e887d0806f00f617737a4d367711686e98
ThangPH <hathang2004@gmail.com>
2025-08-26 21:23:35 +0700

    update product category client

--- controllers/client/products.controller.js
+++ controllers/client/products.controller.js
@@ -1,5 +1,7 @@
 const productHelper = require("../../helpers/product");
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
+const productCategoryHelper = require("../../helpers/product-category");
 // [GET] /products
 module.exports.index = async (req, res) => {
   const products = await Product.find({
@@ -23,5 +25,27 @@ module.exports.detail = async (req, res) => {
   res.render("client/pages/products/detail", {
     pageTitle: product.title,
     product: product,
+  });
+};
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+  const category = await ProductCategory.findOne({
+    deleted: false,
+    slug: req.params.slugCategory,
+  });
+
+  const listSubCategory = (
+    await productCategoryHelper.getSubCategory(category.id)
+  ).map((item) => item.id);
+
+  const products = await Product.find({
+    status: "active",
+    deleted: false,
+    product_category_id: { $in: [category.id, ...listSubCategory] },
+  }).sort({ position: "desc" });
+
+  res.render("client/pages/products/index", {
+    pageTitle: category.title,
+    products: products,
   });
 };
