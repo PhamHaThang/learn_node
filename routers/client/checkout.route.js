ceb2081950a0023f007ba0a4d5ce25e9dcc7d180
ThangPH <hathang2004@gmail.com>
2025-08-28 21:05:03 +0700

    checkout success page

---
+++ routers/client/checkout.route.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../../controllers/client/checkout.controller");
+// [GET] /checkout
+router.get("/", controller.index);
+// [POST] /checkout/order
+router.post("/order", controller.order);
+// [GET] /checkout/success/:orderId
+router.get("/success/:orderId", controller.success);
+module.exports = router;
