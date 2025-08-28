e5a3c2782875b005dbf4e51d5b26e710e3a30c4b
ThangPH <hathang2004@gmail.com>
2025-08-28 21:28:17 +0700

    register

--- routers/client/index.route.js
+++ routers/client/index.route.js
@@ -3,6 +3,7 @@ const homeRoutes = require("./home.route");
 const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
 const checkoutRoutes = require("./checkout.route");
+const userRoutes = require("./user.route");
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 const cartMiddleware = require("../../middlewares/client/cart.middleware");
 module.exports = (app) => {
@@ -13,4 +14,5 @@ module.exports = (app) => {
   app.use("/search", searchRoutes);
   app.use("/cart", cartRoutes);
   app.use("/checkout", checkoutRoutes);
+  app.use("/user", userRoutes);
 };
