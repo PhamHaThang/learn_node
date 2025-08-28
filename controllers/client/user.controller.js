b36c4c109055a2f66a2854816559275c4f26299b
ThangPH <hathang2004@gmail.com>
2025-08-28 21:45:55 +0700

    login + logout

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -21,3 +21,36 @@ module.exports.registerPost = async (req, res) => {
   res.cookie("tokenUser", user.tokenUser);
   res.redirect("/");
 };
+// [GET] /user/login
+module.exports.login = async (req, res) => {
+  res.render("client/pages/user/login", {
+    pageTitle: "Đăng nhập",
+  });
+};
+// [POST] /user/login
+module.exports.loginPost = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email: email, deleted: false });
+  if (!user) {
+    req.flash("error", "Email không tồn tại");
+    res.redirect(req.get("Referrer") || "/user/login");
+    return;
+  }
+  if (md5(password) !== user.password) {
+    req.flash("error", "Sai mật khẩu");
+    res.redirect(req.get("Referrer") || "/user/login");
+    return;
+  }
+  if (user.status === "inactive") {
+    req.flash("error", "Tài khoản đang bị kháo");
+    res.redirect(req.get("Referrer") || "/user/login");
+    return;
+  }
+  res.cookie("tokenUser", user.tokenUser);
+  res.redirect("/");
+};
+// [GET] /user/logout
+module.exports.logout = async (req, res) => {
+  res.clearCookie("tokenUser");
+  res.redirect("/");
+};
--- routers/client/index.route.js
+++ routers/client/index.route.js
@@ -5,10 +5,12 @@ const cartRoutes = require("./cart.route");
 const checkoutRoutes = require("./checkout.route");
 const userRoutes = require("./user.route");
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
+const userMiddleware = require("../../middlewares/client/user.middleware");
 const cartMiddleware = require("../../middlewares/client/cart.middleware");
 module.exports = (app) => {
   app.use(categoryMiddleware.category);
   app.use(cartMiddleware.cartId);
+  app.use(userMiddleware.infoUser);
   app.use("/", homeRoutes);
   app.use("/products", productRoutes);
   app.use("/search", searchRoutes);
